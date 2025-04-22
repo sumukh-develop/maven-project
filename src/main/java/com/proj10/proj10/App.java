337d0b3e430fcc65f6e6becd0a5e8bb492564be8
sumukh-develop <sumukhsumukh01@gmail.com>
2025-04-22 09:36:02 +0530

    first commit

---
+++ src/main/java/com/proj10/proj10/App.java
@@ -0,0 +1,13 @@
+package com.proj10.proj10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
